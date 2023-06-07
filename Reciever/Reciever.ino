e943620973cad7f3d56396f151f29729e6c26644
Tunanika <70443354+Tunanika@users.noreply.github.com>
2023-06-07 21:29:54 +0300

    first commit

---
+++ Reciever/Reciever.ino
@@ -0,0 +1,97 @@
+#include "WiFi.h"
+#include <esp_now.h>
+#include <ESP32Servo.h>
+
+Servo motor1;
+Servo motor2;
+Servo motor3;
+
+int Motor1PWM = 0;
+int Motor2PWM = 0;
+int Motor3PWM = 0;
+
+float tempX = 0.0f;
+float tempY = 0.0f;
+
+float tempMotorR = 0.0f;
+float tempMotorL = 0.0f;
+
+int lastMsg = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.mode(WIFI_MODE_STA);
+    Serial.println(WiFi.macAddress());
+    if (esp_now_init() != ESP_OK)
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    motor1.attach(18);
+    motor2.attach(19);
+    motor3.attach(21);
+
+    Serial.println("Init recieve");
+    esp_now_register_recv_cb(OnDataRecv);
+}
+struct data_frame
+{
+    int pot1;
+    int pot2;
+    int pot3;
+    int pot4;
+};
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+    data_frame ControllerData = {};
+    memcpy(&ControllerData, incomingData, len);
+
+    // Motor1PWM = map(ControllerData.pot1, 0, 4096, 1000, 2000);
+    // Motor2PWM = map(ControllerData.pot2, 0, 4096, 1000, 2000);
+    Motor3PWM = map(ControllerData.pot3, 0, 4096, 1000, 2000);
+
+    tempX = mapfloat(ControllerData.pot1, 0, 4096, -1, 1);
+    tempY = mapfloat(ControllerData.pot2, 0, 4096, -1, 1);
+
+    tempMotorR = tempY - tempX;
+    tempMotorL = tempY + tempX;
+
+    Motor1PWM = mapfloat(tempMotorR, -1, 1, 1000, 2000);
+    Motor2PWM = mapfloat(tempMotorL, -1, 1, 1000, 2000);
+
+    Serial.println(millis());
+    Serial.println("Pot1: " + String(ControllerData.pot1));
+    Serial.println("Pot2: " + String(ControllerData.pot2));
+    Serial.println("Motor1 PWM val: " + String(Motor1PWM));
+    Serial.println("Motor2 PWM val: " + String(Motor2PWM));
+    Serial.println("Motor3 PWM val: " + String(Motor3PWM));
+
+    motor1.writeMicroseconds(Motor1PWM);
+    motor2.writeMicroseconds(Motor2PWM);
+    motor3.writeMicroseconds(Motor3PWM);
+    lastMsg = millis();
+}
+void loop()
+{
+    delay(1000);
+    if (millis() - 100 > lastMsg)
+    {
+        Serial.println("Holy Crappy");
+        killAll();
+    }
+}
+
+void killAll()
+{
+    motor1.writeMicroseconds(1500);
+    motor2.writeMicroseconds(1500);
+    motor3.writeMicroseconds(1500);
+    delay(500);
+    Serial.println("I BE DEAD!");
+    ESP.restart();
+}
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
---
+++ Sender/Sender.ino
@@ -0,0 +1,82 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>             //SPI library for TFT screen
+
+#define TFT_CS 17
+#define TFT_RST 4
+#define TFT_DC 16
+
+uint8_t receiver_mac[6] = {0x40, 0x22, 0xD8, 0x05, 0x43, 0x88};
+// 40:22:D8:05:43:88
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+float vBat = 0.0f;
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.mode(WIFI_MODE_STA);
+    Serial.println(WiFi.macAddress());
+    esp_now_init();
+
+    esp_now_peer_info_t peer_info = {};
+    peer_info.channel = 1;
+    peer_info.encrypt = false;
+    memcpy(peer_info.peer_addr, receiver_mac, 6);
+    esp_now_add_peer(&peer_info);
+
+    Serial.println("Init Sender");
+    tft.initR(INITR_BLACKTAB); // Init ST7735S chip, black tab
+
+    tft.setSPISpeed(50000000);
+    tft.fillScreen(ST77XX_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextSize(2);
+    tft.print("init");
+    Serial.println(F("Initialized"));
+    delay(800);
+    tft.fillScreen(ST77XX_BLACK);
+}
+
+char charVal[6];
+
+struct data_frame
+{
+    int pot1;
+    int pot2;
+    int pot3;
+    int pot4;
+};
+
+void loop()
+{
+    data_frame data = {};
+    data.pot1 = analogRead(32);
+    data.pot2 = analogRead(33);
+    data.pot3 = analogRead(34);
+    data.pot4 = analogRead(35);
+
+    vBat = (mapfloat(analogRead(36), 0, 4096, 0, 3.3) + 0.1) / 0.6875;
+
+    Serial.println(analogRead(36));
+    Serial.println("vBat: ");
+    Serial.println(vBat);
+
+    tft.setCursor(0, 0);
+    tft.setTextSize(2);
+    tft.print("Battery   Voltage:  ");
+    tft.print(dtostrf(vBat, 3, 1, charVal));
+
+    esp_now_send(receiver_mac, (uint8_t *)&data, sizeof(data));
+    delay(60);
+    tft.fillScreen(ST77XX_BLACK);
+}
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
